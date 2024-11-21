90ff5ed0ceff32c06f559a8e26d501ef8984920c
shivam chaturvedi <chaturvedishivam598@gmail.com>
2024-11-21 13:47:11 +0530

    safe hash

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,4 +1,4 @@
-export const BACKEND_SERVER_URL="http://localhost:8080";
+export const BACKEND_SERVER_URL="http://192.168.151.103:8080";
 
 // const API='https://backend-health-bestie.vercel.app/users/conversations';
 
