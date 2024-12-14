a24d0336fa7913a65b935cef887811cc1d611757
shivam chaturvedi <chaturvedishivam598@gmail.com>
2024-12-14 21:57:14 +0530

    doamin updated

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,4 +1,4 @@
-export const BACKEND_SERVER_URL="http://localhost:8080";
+export const BACKEND_SERVER_URL="https://retailedgebe.duckdns.org:8443";
 
 // https://retail-edge-be-latest.onrender.com/
 
